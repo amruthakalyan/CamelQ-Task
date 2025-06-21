ae7a6a271ef5c70efa12c7f8c12a021ffe167c6f
kalyan <116357692+amruthakalyan@users.noreply.github.com>
2025-06-21 08:44:24 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+
 import App from './App';
 import { CartProvider } from './context/CartContext';
 import './main.css';
